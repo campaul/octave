c19213baa8028b9a672e9d6223e16acc3ffa2335
Ben Bariteau <firba1@gmail.com>
2014-10-05 10:37:44 -0700

    Added assembler

---
+++ assemble/assembler/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/campaul/octave/assemble"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Println("Usage: %v [assembly_file] [output_file]", args[0])
+		return
+	}
+	file, err := os.Open(args[1])
+	if err != nil {
+		fmt.Println("Unable to open file for reading: ", err)
+		return
+	}
+	out, err := assemble.Assemble(file)
+	if err != nil {
+		fmt.Println("Error while assembling:", err)
+		return
+	}
+	outfile, err := os.Create(args[2])
+	if err != nil {
+		fmt.Println("Unable to open file for writing: ", err)
+		return
+	}
+	_, err = outfile.Write(out)
+	if err != nil {
+		fmt.Println("Error while writing to file")
+		return
+	}
+}
